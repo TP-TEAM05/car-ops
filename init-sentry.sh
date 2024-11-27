f6fad7a4a983b93db588edcf5e0da9093c5604a5
ReCo DEV <recoteamfiit@gmail.com>
2024-11-27 18:24:52 +0000

    Sentry init script

--- init-sentry.sh
+++ init-sentry.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
 curl -sL https://sentry.io/get-cli/ | bash
-sudo echo "export SENTRY_DSN=https://60b54799df74ddab5585ba77f35f37ab@o4508080703864832.ingest.de.sentry.io/4508363861655632" >> /etc/environment
+
+export SENTRY_DSN="https://60b54799df74ddab5585ba77f35f37ab@o4508080703864832.ingest.de.sentry.io/4508363861655632"
+sentry-cli update
+eval "$(sentry-cli bash-hook)"
