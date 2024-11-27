0bb17a39c2244b6200074b9a5846cdc061998bc5
Michal Magula <m.magula23@gmail.com>
2024-11-27 19:08:03 +0100

    init sentry script

---
+++ init-sentry.sh
@@ -0,0 +1,2 @@
+curl -sL https://sentry.io/get-cli/ | bash
+sudo echo "export SENTRY_DSN=https://60b54799df74ddab5585ba77f35f37ab@o4508080703864832.ingest.de.sentry.io/4508363861655632" >> /etc/environment
