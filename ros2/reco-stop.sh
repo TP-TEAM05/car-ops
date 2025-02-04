be40e05fd8a51894b6ebabdc4d360ee3dafc1e1d
ReCo <recoteamfiit@gmail.com>
2025-02-04 08:36:43 +0000

    update pipeline

--- ros2/reco-stop.sh
+++ ros2/reco-stop.sh
@@ -2,10 +2,14 @@
 
 # ReCo script to kill all processes
 
-# Kills all ROS processes 
-kill -9 $(ps -aux | grep ros |awk '{print $2}' | head -6)
+# Kills all ROS processes
+kill -9 $(ps -aux | grep ros | awk '{print $2}' | head -6)
 
 # Kills all rpi_controller processes
 kill -9 $(ps -aux | grep 'rpi_controller' | awk '{print $2}' | head -3)
 
-echo "RECO nodes killed at: $(date)" >> /home/ubuntu/log/reco.log
+# If ros2_ws is running in screen kill the screen
+screen -X -S ros2_ws quit
+screen -wipe
+
+echo "RECO nodes killed at: $(date)" >>/home/ubuntu/log/reco.log
